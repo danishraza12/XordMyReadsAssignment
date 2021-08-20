3a5f641200599beb5ebf34804ad86e2ff292b6ec
Danish Raza <60378949+danishraza12@users.noreply.github.com>
2021-08-20 21:01:43 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="list-books-title">
-          <h1>MyReads</h1>
+          <h1>Xord MyReads assignment by Danish Raza</h1>
         </div>
     )
 }
